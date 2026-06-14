161efc9297e474001a5954f3d98700342ce02a30
olimali <mingboyevalisher171@gmail.com>
2026-06-14 01:06:42 -0700

    fix: update build script in package.json and allow lastMessage to be null in chat model

--- server/src/models/chat.model.ts
+++ server/src/models/chat.model.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema, type Document } from "mongoose";
 
 export interface IChat extends Document {
   participants: mongoose.Types.ObjectId[];
-  lastMessage?: mongoose.Types.ObjectId;
+  lastMessage?: mongoose.Types.ObjectId | null;
   lastMessageAt?: Date;
   createdAt: Date;
   updatedAt: Date;
